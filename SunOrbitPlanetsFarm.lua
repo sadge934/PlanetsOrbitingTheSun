7dbd991e3a8abe7c276d2878138b2ac0d5ffa849
sadge934 <104523298+sadge934@users.noreply.github.com>
2022-08-07 13:09:10 +0200

    Add files via upload

---
+++ SunOrbitPlanetsFarm.lua
@@ -0,0 +1,91 @@
+getgenv().AutoSunOrbitingPlanets = state
+
+spawn(function()
+    while AutoSunOrbitingPlanets == true do
+        local uhOh1 = game:GetService("Workspace").World.Sun.orbitingBodies.Mercury
+        local uhOh2 = -2.e+22
+        game:GetService("ReplicatedStorage").ChangePlanetMass:FireServer(uhOh1, uhOh2)
+        wait(5)
+    end
+end)
+
+spawn(function()
+    while AutoSunOrbitingPlanets == true do
+        local uhOh1 = game:GetService("Workspace").World.Sun.orbitingBodies.Venus
+        local uhOh2 = -2.e+22
+        game:GetService("ReplicatedStorage").ChangePlanetMass:FireServer(uhOh1, uhOh2)
+        wait(5)
+    end
+end)
+
+spawn(function()
+    while AutoSunOrbitingPlanets == true do
+        local uhOh1 = game:GetService("Workspace").World.Sun.orbitingBodies.Jupiter
+        local uhOh2 = -2.e+22
+        game:GetService("ReplicatedStorage").ChangePlanetMass:FireServer(uhOh1, uhOh2)
+        wait(5)
+    end
+end)
+
+spawn(function()
+    while AutoSunOrbitingPlanets == true do
+        local uhOh1 = game:GetService("Workspace").World.Sun.orbitingBodies.Ceres
+        local uhOh2 = -2.e+22
+        game:GetService("ReplicatedStorage").ChangePlanetMass:FireServer(uhOh1, uhOh2)
+        wait(5)
+    end
+end)
+
+spawn(function()
+    while AutoSunOrbitingPlanets == true do
+        local uhOh1 = game:GetService("Workspace").World.Sun.orbitingBodies.Mars
+        local uhOh2 = -2.e+22
+        game:GetService("ReplicatedStorage").ChangePlanetMass:FireServer(uhOh1, uhOh2)
+        wait(5)
+    end
+end)
+
+spawn(function()
+    while AutoSunOrbitingPlanets == true do
+        local uhOh1 = game:GetService("Workspace").World.Sun.orbitingBodies.Earth
+        local uhOh2 = -2.e+22
+        game:GetService("ReplicatedStorage").ChangePlanetMass:FireServer(uhOh1, uhOh2)
+        wait(5)
+    end
+end)
+
+spawn(function()
+    while AutoSunOrbitingPlanets == true do
+        local uhOh1 = game:GetService("Workspace").World.Sun.orbitingBodies.Uranus
+        local uhOh2 = -2.e+22
+        game:GetService("ReplicatedStorage").ChangePlanetMass:FireServer(uhOh1, uhOh2)
+        wait(5)
+    end
+end)
+
+spawn(function()
+    while AutoSunOrbitingPlanets == true do
+        local uhOh1 = game:GetService("Workspace").World.Sun.orbitingBodies.Neptune
+        local uhOh2 = -2.e+22
+        game:GetService("ReplicatedStorage").ChangePlanetMass:FireServer(uhOh1, uhOh2)
+        wait(5)
+    end
+end)
+
+spawn(function()
+    while AutoSunOrbitingPlanets == true do
+        local uhOh1 = game:GetService("Workspace").World.Sun.orbitingBodies.Pluto
+        local uhOh2 = -2.e+22
+        game:GetService("ReplicatedStorage").ChangePlanetMass:FireServer(uhOh1, uhOh2)
+        wait(5)
+    end
+end)
+
+spawn(function()
+    while AutoSunOrbitingPlanets == true do
+        local uhOh1 = game:GetService("Workspace").World.Sun.orbitingBodies.Saturn
+        local uhOh2 = -2.e+22
+        game:GetService("ReplicatedStorage").ChangePlanetMass:FireServer(uhOh1, uhOh2)
+        wait(5)
+    end
+end)
