260bc0f7ee72d35f466520f757cf38492a162b51
sadge934 <104523298+sadge934@users.noreply.github.com>
2022-08-07 13:33:27 +0200

    Add files via upload

---
+++ AutoPlanetsRobloxiaCentauri.lua
@@ -0,0 +1,100 @@
+getgenv().AutoPlanetsRobloxiaCentauri = state
+
+spawn(function()
+    while AutoPlanetsRobloxiaCentauri == true do
+        local args = {
+        [1] = workspace.World:FindFirstChild("Robloxia Centauri").orbitingBodies.Grimm,
+        [2] = -2.e+22
+                     }
+        game:GetService("ReplicatedStorage").ChangePlanetMass:FireServer(unpack(args))
+        wait(5)
+    end
+end)
+
+spawn(function()
+    while AutoPlanetsRobloxiaCentauri == true do
+        local args = {
+        [1] = workspace.World:FindFirstChild("Robloxia Centauri").orbitingBodies.Fourier,
+        [2] = -2.e+22
+                     }
+        game:GetService("ReplicatedStorage").ChangePlanetMass:FireServer(unpack(args))
+        wait(5)
+    end
+end)
+
+spawn(function()
+    while AutoPlanetsRobloxiaCentauri == true do
+        local args = {
+        [1] = workspace.World:FindFirstChild("Robloxia Centauri").orbitingBodies.Curie,
+        [2] = -2.e+22
+                     }
+        game:GetService("ReplicatedStorage").ChangePlanetMass:FireServer(unpack(args))
+        wait(5)
+    end
+end)
+
+spawn(function()
+    while AutoPlanetsRobloxiaCentauri == true do
+        local args = {
+        [1] = workspace.World:FindFirstChild("Robloxia Centauri").orbitingBodies.Euler,
+        [2] = -2.e+22
+                     }
+        game:GetService("ReplicatedStorage").ChangePlanetMass:FireServer(unpack(args))
+        wait(5)
+    end
+end)
+
+spawn(function()
+    while AutoPlanetsRobloxiaCentauri == true do
+        local args = {
+        [1] = workspace.World:FindFirstChild("Robloxia Centauri").orbitingBodies.Sohl,
+        [2] = -2.e+22
+                     }
+        game:GetService("ReplicatedStorage").ChangePlanetMass:FireServer(unpack(args))
+        wait(5)
+    end
+end)
+
+spawn(function()
+    while AutoPlanetsRobloxiaCentauri == true do
+        local args = {
+        [1] = workspace.World:FindFirstChild("Robloxia Centauri").orbitingBodies.Bohr,
+        [2] = -2.e+22
+                     }
+        game:GetService("ReplicatedStorage").ChangePlanetMass:FireServer(unpack(args))
+        wait(5)
+    end
+end)
+
+spawn(function()
+    while AutoPlanetsRobloxiaCentauri == true do
+        local args = {
+        [1] = workspace.World:FindFirstChild("Robloxia Centauri").orbitingBodies.Euler,
+        [2] = -2.e+22
+                     }
+        game:GetService("ReplicatedStorage").ChangePlanetMass:FireServer(unpack(args))
+        wait(5)
+    end
+end)
+
+spawn(function()
+    while AutoPlanetsRobloxiaCentauri == true do
+        local args = {
+        [1] = workspace.World:FindFirstChild("Robloxia Centauri").orbitingBodies.Newton,
+        [2] = -2.e+22
+                     }
+        game:GetService("ReplicatedStorage").ChangePlanetMass:FireServer(unpack(args))
+        wait(5)
+    end
+end)
+
+spawn(function()
+    while AutoPlanetsRobloxiaCentauri == true do
+        local args = {
+        [1] = workspace.World:FindFirstChild("Robloxia Centauri").orbitingBodies.Hawkins,
+        [2] = -2.e+22
+                     }
+        game:GetService("ReplicatedStorage").ChangePlanetMass:FireServer(unpack(args))
+        wait(5)
+    end
+end)
